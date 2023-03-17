ff3ae566ccb13e15b0afeb95f8935c421481fec6
Maryna Tsuk <73252923+marynatsuk@users.noreply.github.com>
2023-03-17 20:49:59 +0200

    Create Counter.java

---
+++ laboratory 1/task 6/Counter.java
@@ -0,0 +1,35 @@
+package com.company.task_6;
+
+public class Counter {
+    private int value = 0;
+    private final Object lock = new Object();
+
+    public synchronized void increment() {
+        value++;
+    }
+
+    public synchronized void decrement() {
+        value--;
+    }
+
+    public void incSyncBlock() {
+        synchronized (lock) {
+            value++;
+        }
+    }
+    public void decSyncBlock() {
+        synchronized (lock) {
+            value--;
+        }
+    }
+
+    public synchronized int getValue() {
+        return value;
+    }
+    public void badInc() {
+        value++;
+    }
+    public void badDec() {
+        value--;
+    }
+}
