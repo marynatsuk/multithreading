12e5756f0b2116635f6845fbe269f3993d0660f2
Maryna Tsuk <73252923+marynatsuk@users.noreply.github.com>
2023-03-17 09:50:50 +0200

    Add files via upload

---
+++ laboratory 1/task 4/BallCanvas.java
@@ -0,0 +1,34 @@
+package com.company.task_4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class BallCanvas extends JPanel {
+    private ArrayList<Ball> balls = new ArrayList<>();
+
+    public void stopBalls() {
+        for (Ball ball : balls) {
+            ball.stopBall();
+        }
+    }
+
+    public void runBalls() {
+        for (Ball ball : balls) {
+            ball.runBall();
+        }
+    }
+
+    public void add(Ball b) {
+        this.balls.add(b);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        for (Ball b : balls) {
+            b.draw(g2);
+        }
+    }
+}
