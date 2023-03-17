48d63fa42cf344d56e36c7dc3161c42f84b67196
Maryna Tsuk <73252923+marynatsuk@users.noreply.github.com>
2023-03-17 09:54:01 +0200

    Add files via upload

---
+++ laboratory 1/task 6/Counter.java
@@ -0,0 +1,34 @@
+package com.company.task_6;
+
+public class Counter {
+    private int count = 0;
+    private final Object lock = new Object();
+    public synchronized void increment() {
+        count++;
+    }
+
+    public synchronized void decrement() {
+        count--;
+    }
+
+    public void incSyncBlock() {
+        synchronized (lock) {
+            count++;
+        }
+    }
+    public void decSyncBlock() {
+        synchronized (lock) {
+            count--;
+        }
+    }
+
+    public synchronized int getCount() {
+        return count;
+    }
+    public void badInc() {
+        count++;
+    }
+    public void badDec() {
+        count--;
+    }
+}
