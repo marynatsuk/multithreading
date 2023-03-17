98fee555553e335d547574f5f41e4e7b59cebf03
Maryna Tsuk <73252923+marynatsuk@users.noreply.github.com>
2023-03-17 20:49:35 +0200

    Create Main.java

---
+++ laboratory 1/task 6/Main.java
@@ -0,0 +1,138 @@
+package com.company.task_6;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+public class Main {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        notSync(counter);
+        //syncMethod(counter);
+        //syncBlock(counter);
+        //syncObj(counter);
+
+    }
+    private static void notSync(Counter counter) {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100_000; i++) {
+                    counter.badInc();
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100_000; i++) {
+                    counter.badDec();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Not synchronised: " + counter.getValue());
+
+    }
+    private static void syncMethod(Counter counter) {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100_000; i++) {
+                    counter.increment();
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100_000; i++) {
+                    counter.decrement();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Sync Method: " + counter.getValue());
+
+    }
+    private static void syncBlock(Counter counter) {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100_000; i++) {
+                    counter.incSyncBlock();
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100_000; i++) {
+                    counter.decSyncBlock();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Sync Block: " + counter.getValue());
+    }
+    private static void syncObj(Counter counter) {
+        final Lock lock = new ReentrantLock();
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100_000; i++) {
+                    lock.lock();
+                    counter.badInc();
+                    lock.unlock();
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100_000; i++) {
+                    lock.lock();
+                    counter.badDec();
+                    lock.unlock();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Sync Block: " + counter.getValue());
+    }
+
+}
